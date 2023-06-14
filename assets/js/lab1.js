b178a9ac2e72671a86c6dde4c9a6ec392b0259f6
Donitaqueen150075 <97503052+Donitaqueen150075@users.noreply.github.com>
2023-06-14 17:22:13 +0800

    20230614-5

--- assets/js/lab1.js
+++ assets/js/lab1.js
@@ -1,3 +1,4 @@
+const { default: axios } = require("axios");
 var http = require("http");
 
 http.createServer(function (request, response) {
@@ -13,3 +14,6 @@ http.createServer(function (request, response) {
 
 // 终端打印如下信息
 console.log('Server running at http://127.0.0.1:8888/');
+
+
+
